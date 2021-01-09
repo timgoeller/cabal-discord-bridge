423740c16d57dc43bf04480c309f84ec172d10a7
Tim Göller <tim.goeller@posteo.de>
2021-01-09 14:08:51 +0100

    send messages with webhook instead of bot

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var CabalBot = require('cabal-bot-core')
+const CabalBot = require('cabal-bot-core')
 const Discord = require('discord.js')
 
 const fs = require('fs')
-
+let config = {}
 try {
-  var config = JSON.parse(fs.readFileSync('config.json'))
+  config = JSON.parse(fs.readFileSync('config.json'))
 } catch {
   throw new Error('Error while parsing config, make sure config.js exists')
 }
@@ -23,6 +23,7 @@ const cabalOpts = {
   }
 }
 
+const WEBHOOK_NAME = 'Discord-Cabal-Bridge'
 const cabalBot = new CabalBot(config.name, cabalOpts)
 
 cabalBot.joinCabal(config.cabalKey)
@@ -30,19 +31,28 @@ discordBot.login(config.discordSecret)
 
 //* doesn't fully work yet, won't send messages from channel x to all channels yet
 
-discordBot.on('ready', () => {
-  discordBot.on('message', msg => processMessageFromDiscord(msg))
+discordBot.on('ready', async () => {
+  if (!discordBot.guilds.cache.every((guild) => {
+    return guild.me.hasPermission(['MANAGE_WEBHOOKS'])
+  })) {
+    throw new Error('Bot needs MANAGE_WEBHOOKS permission!')
+  }
+
+  const hooks = new WebhookController()
+  hooks.getOrCreateWebhooks(discordBot)
+  discordBot.on('message', msg => processMessageFromDiscord(msg, hooks))
+  cabalBot.on('new-message', (envelope) => processMessageFromCabal(envelope, hooks))
 })
 
-cabalBot.on('new-message', (envelope) => processMessageFromCabal(envelope))
-
-function processMessageFromDiscord(msg) {
-  if(discordBot.user.id !== msg.author.id) {
+function processMessageFromDiscord (msg, hooks) {
+  if (discordBot.user.id !== msg.author.id && !hooks.sentByBot(msg)) {
     const cabalChannelsToForwardTo = new Set()
     config.mappings.forEach(mapping => {
       if (mapping.from === 'discord' || mapping.from === 'both') {
         if (mapping.discord.includes('*') || mapping.discord.includes(msg.channel.name)) {
-          mapping.cabal.forEach(cabalMapping => cabalChannelsToForwardTo.add(cabalMapping))
+          mapping.cabal.forEach(cabalMapping =>
+            cabalChannelsToForwardTo.add(cabalMapping)
+          )
         }
       }
     })
@@ -50,7 +60,7 @@ function processMessageFromDiscord(msg) {
   }
 }
 
-function processMessageFromCabal(envelope) {
+function processMessageFromCabal (envelope, hooks) {
   const discordChannelsToForwardTo = new Set()
   config.mappings.forEach(mapping => {
     if (mapping.from === 'cabal' || mapping.from === 'both') {
@@ -60,9 +70,35 @@ function processMessageFromCabal(envelope) {
     }
   })
 
-  discordBot.channels.cache.forEach(discordChannel => {
-    if (discordChannelsToForwardTo.has(discordChannel.name)) {
-      discordChannel.send(envelope.message.value.content.text)
+  hooks.webhooks.forEach((webhook) => {
+    if (discordChannelsToForwardTo.has(webhook.channelName)) {
+      webhook.send(envelope.message.value.content.text, {
+        username: envelope.author.name + '@cabal'
+      })
     }
   })
+}
+
+class WebhookController {
+  async getOrCreateWebhooks (client) {
+    const webhookPromises = []
+    client.channels.cache.forEach(channel => {
+      if (channel.type === 'text') {
+        webhookPromises.push(async () => {
+          const hooks = await channel.fetchWebhooks()
+          let hook = hooks.find(h => h.name === WEBHOOK_NAME)
+          if (!hook) {
+            hook = await channel.createWebhook(WEBHOOK_NAME)
+          }
+          hook.channelName = channel.name
+          return hook
+        })
+      }
+    })
+    this.webhooks = await Promise.all(webhookPromises.map(fn => fn()))
+  }
+
+  sentByBot (msg) {
+    return this.webhooks.some(hook => hook.id === msg.author.id)
+  }
 }
